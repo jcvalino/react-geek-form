9784b0784cd9650cade1c00fa9872df857f08d60
jericovalino15 <jcvalino@multisyscorp.com>
2023-09-01 14:26:29 +0800

    :tada: initial commit
    add createForm utility

---
+++ src/utils/createForm/index.tsx
@@ -0,0 +1,167 @@
+import React, {
+  useMemo,
+  useEffect,
+  useContext,
+  useCallback,
+  createContext,
+} from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import type { FieldValues, UseFormProps, UseFormReturn } from 'react-hook-form';
+
+type FormContext<TFieldValues extends FieldValues> =
+  UseFormReturn<TFieldValues>;
+
+type FieldContext = 'register' | 'control' | 'error';
+
+type JsxForm = React.DetailedHTMLProps<
+  React.FormHTMLAttributes<HTMLFormElement>,
+  HTMLFormElement
+>;
+
+const createForm = <TSchema extends z.ZodObject<any> | z.ZodEffects<any>>({
+  zodSchema,
+}: {
+  zodSchema: TSchema;
+}) => {
+  const FormContext = createContext<null | FormContext<z.infer<TSchema>>>(null);
+
+  const Form = ({
+    children,
+    onSubmit,
+    defaultFormValues,
+    onValidatedSubmit,
+    ...formProps
+  }: {
+    [K in keyof JsxForm]: K extends 'children'
+      ?
+          | React.ReactNode
+          | ((
+              useFormReturns: UseFormReturn<z.infer<TSchema>>
+            ) => React.ReactNode)
+      : JsxForm[K];
+  } & {
+    defaultFormValues?: Record<any, any>;
+    onValidatedSubmit: (values: z.infer<TSchema>) => void;
+  }) => {
+    const { handleSubmit, register, control, ...rest } = useFormContext();
+    return (
+      <form
+        {...formProps}
+        onSubmit={(e) => {
+          onSubmit?.(e);
+          handleSubmit(onValidatedSubmit)(e);
+        }}
+      >
+        {typeof children === 'function'
+          ? children({
+              handleSubmit,
+              register,
+              control,
+              ...rest,
+            } as any)
+          : children}
+      </form>
+    );
+  };
+
+  const useFormContext = () => {
+    const context = useContext(FormContext);
+    if (!context) throw new Error('Invalid use');
+    return context;
+  };
+
+  let useFormPropsRef: Omit<UseFormProps<z.infer<TSchema>>, 'resolver'> | null =
+    null;
+
+  type Ctx = UseFormReturn<z.infer<TSchema>> & {
+    setFormConfigs: (
+      props: Omit<UseFormProps<z.infer<TSchema>>, 'resolver'>
+    ) => void;
+  };
+
+  const forwardContext = <
+    TWrappedComponent extends (props: any, ctx: Ctx) => JSX.Element
+  >(
+    component: TWrappedComponent
+  ) => {
+    return (
+      props: React.ComponentPropsWithoutRef<TWrappedComponent> & {
+        onInitializedFormContext?: (ctx: Ctx) => void;
+      }
+    ) => {
+      const setFormConfigs = useCallback(
+        (props: UseFormProps<z.infer<TSchema>>) => {
+          useFormPropsRef = props;
+        },
+        []
+      );
+
+      const form = useForm({
+        resolver: zodResolver(zodSchema),
+        ...(useFormPropsRef ?? {}),
+      });
+
+      const ctx = useMemo(
+        () => ({
+          ...form,
+          setFormConfigs,
+        }),
+        [form, setFormConfigs]
+      );
+
+      useEffect(() => {
+        props.onInitializedFormContext?.(ctx);
+      }, [ctx, props]);
+
+      return (
+        <FormContext.Provider value={form}>
+          {component(props, ctx)}
+        </FormContext.Provider>
+      );
+    };
+  };
+
+  const withContext = <
+    TWrappedFormField extends (...props: any[]) => JSX.Element
+  >(
+    WrappedFormField: TWrappedFormField
+  ) => {
+    return (remainingProps: {
+      [K in keyof Omit<
+        React.ComponentPropsWithoutRef<TWrappedFormField>,
+        FieldContext
+      >]: K extends 'name'
+        ? keyof z.infer<TSchema>
+        : Omit<
+            React.ComponentPropsWithoutRef<TWrappedFormField>,
+            FieldContext
+          >[K];
+    }) => {
+      const {
+        control,
+        register,
+        formState: { errors },
+      } = useFormContext();
+      return (
+        // @ts-expect-error
+        <WrappedFormField
+          control={control}
+          register={register}
+          error={errors?.[remainingProps?.name]}
+          {...remainingProps}
+        />
+      );
+    };
+  };
+
+  return {
+    Form,
+    withContext,
+    forwardContext,
+  };
+};
+
+export default createForm;
