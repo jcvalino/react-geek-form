7d94e78df25dfb1a2808d4938adae0eb4de37779
jcvalino <jcvalino@multisyscorp.com>
2025-03-24 14:39:54 +0800

    :hammer: update sandbox

--- apps/sandbox/src/components/FormInput.tsx
+++ apps/sandbox/src/components/FormInput.tsx
@@ -1,5 +1,5 @@
 import { type IconType } from 'react-icons';
-import { type ComponentPropsWithoutRef, useMemo } from 'react';
+import { type ComponentPropsWithoutRef, forwardRef, useMemo } from 'react';
 
 import { cn } from './../utilities';
 
@@ -16,70 +16,76 @@ type Props = {
   onChange: (value: string) => void;
 } & Omit<ComponentPropsWithoutRef<typeof FormFieldWrapper>, 'children'>;
 
-function FormInput({
-  name,
-  value,
-  label,
-  error,
-  icon: Icon,
-  type = 'text',
-  noError = false,
-  optional = false,
-  disabled = false,
-  readOnly = false,
-  testId = undefined,
-  maxLength = undefined,
-  placeholder = undefined,
-  errorDescription = undefined,
-  onChange,
-}: Props) {
-  const hasIcon = useMemo(() => Boolean(Icon), [Icon]);
-  return (
-    <FormFieldWrapper
-      name={name}
-      error={error}
-      label={label}
-      noError={noError}
-      readOnly={readOnly}
-      optional={optional && !readOnly}
-      errorDescription={errorDescription}
-    >
-      <div className="relative w-full">
-        {Icon ? (
-          <Icon
+const FormInput = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      name,
+      value,
+      label,
+      error,
+      icon: Icon,
+      type = 'text',
+      noError = false,
+      optional = false,
+      disabled = false,
+      readOnly = false,
+      testId = undefined,
+      maxLength = undefined,
+      placeholder = undefined,
+      errorDescription = undefined,
+      onChange,
+    },
+    ref
+  ) => {
+    const hasIcon = useMemo(() => Boolean(Icon), [Icon]);
+    return (
+      <FormFieldWrapper
+        name={name}
+        error={error}
+        label={label}
+        noError={noError}
+        readOnly={readOnly}
+        optional={optional && !readOnly}
+        errorDescription={errorDescription}
+      >
+        <div className="relative w-full">
+          {Icon ? (
+            <Icon
+              className={cn(
+                'absolute left-[0.625rem] top-1/2 h-[0.875rem] w-[0.875rem] -translate-y-1/2 text-subtle',
+                disabled ? 'text-disabled' : '',
+                error ? 'text-onDanger-subtle' : ''
+              )}
+            />
+          ) : null}
+          <input
+            ref={ref}
+            id={name}
+            type={type}
+            data-test-id={testId}
+            value={value}
             className={cn(
-              'absolute left-[0.625rem] top-1/2 h-[0.875rem] w-[0.875rem] -translate-y-1/2 text-subtle',
-              disabled ? 'text-disabled' : '',
-              error ? 'text-onDanger-subtle' : ''
+              'w-full rounded border bg-white p-[0.625rem] text-sm leading-4 text placeholder:text-placeholder',
+              'focus:outline-none disabled:bg-interface-disabled ',
+              '[&:not(:disabled)]:read-only:border-none [&:not(:disabled)]:read-only:bg-transparent [&:not(:disabled)]:read-only:px-0 [&:not(:disabled)]:read-only:pt-1.5 ',
+              hasIcon ? 'pl-[1.6rem]' : '',
+              error
+                ? 'border-danger-subtle bg-danger-subtle text-onDanger-subtle'
+                : 'focus:border-selected'
             )}
+            onChange={(e) => {
+              const { value } = e.target;
+              if (typeof onChange === 'function') onChange(value);
+            }}
+            placeholder={readOnly ? '-' : placeholder}
+            disabled={readOnly ? false : disabled}
+            readOnly={readOnly}
+            maxLength={maxLength}
           />
-        ) : null}
-        <input
-          id={name}
-          type={type}
-          data-test-id={testId}
-          value={value}
-          className={cn(
-            'w-full rounded border bg-white p-[0.625rem] text-sm leading-4 text placeholder:text-placeholder',
-            'focus:outline-none disabled:bg-interface-disabled ',
-            '[&:not(:disabled)]:read-only:border-none [&:not(:disabled)]:read-only:bg-transparent [&:not(:disabled)]:read-only:px-0 [&:not(:disabled)]:read-only:pt-1.5 ',
-            hasIcon ? 'pl-[1.6rem]' : '',
-            error
-              ? 'border-danger-subtle bg-danger-subtle text-onDanger-subtle'
-              : 'focus:border-selected'
-          )}
-          onChange={(e) => {
-            const { value } = e.target;
-            if (typeof onChange === 'function') onChange(value);
-          }}
-          placeholder={readOnly ? '-' : placeholder}
-          disabled={readOnly ? false : disabled}
-          readOnly={readOnly}
-          maxLength={maxLength}
-        />
-      </div>
-    </FormFieldWrapper>
-  );
-}
+        </div>
+      </FormFieldWrapper>
+    );
+  }
+);
 
 export default FormInput;
